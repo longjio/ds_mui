6693c68e39a8c26cbed203b8440392a2d6767fe2
longjio <longjiyoung@gmail.com>
2025-06-23 19:11:26 +0900

    다크/라이트 변환 위한 테마설정 수정

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,8 @@ import { ThemeProvider } from '@mui/material/styles'; // createTheme은 theme.ts
 import CssBaseline from '@mui/material/CssBaseline';
 
 import { routesConfig } from './routes'; // 분리된 라우트 설정 임포트
-import theme from './theme'; // 분리된 theme.ts 파일에서 테마 객체 임포트
+// import theme from './theme';
+import { getTheme } from './theme';
 
 // AppRoutes 컴포넌트 (useRoutes 훅 사용)
 const AppRoutes = () => {
@@ -14,13 +15,10 @@ const AppRoutes = () => {
 
 function App() {
   return (
-      <ThemeProvider theme={theme}>
-        <CssBaseline /> {/* MUI의 기본 CSS 리셋 및 스타일 적용 */}
-        {/*<Router>*/}
-        {/*  <AppRoutes /> /!* 라우트 렌더링 *!/*/}
-        {/*</Router>*/}
+      <ThemeProvider theme={getTheme('light')}>
+          <CssBaseline />
           <Router basename={process.env.PUBLIC_URL}>
-              <AppRoutes /> {/* 라우트 렌더링 */}
+              <AppRoutes />
           </Router>
       </ThemeProvider>
   );
